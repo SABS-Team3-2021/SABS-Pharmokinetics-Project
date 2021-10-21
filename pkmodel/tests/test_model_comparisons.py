b5f4a84350116fb5e5770164d52bdd6652aad1f9
NicholasFan235 <30301848+NicholasFan235@users.noreply.github.com>
2021-10-21 14:36:46 +0100

    Model comparison tests (#50)
    * Added Model Comparison Tests
    
    * Fixed basic tests
    
    * Updated Comparison Tests
    
    * Resolved Conflicts

--- pkmodel/tests/test_model_comparisons.py
+++ pkmodel/tests/test_model_comparisons.py
@@ -11,10 +11,11 @@ ivSubjects = [pk.IvModelScipy, pk.IVModelBckEuler]
 subSubjects = [pk.SubModelScipy, pk.SubModelBckEuler]
 
 def generateIVParams(reps: int):
-    return [pk.IV_Parameters(Q_pc=random.random(),V_c=random.random(),V_p=random.random(),CL=random.random(),q_c0=random.random(),q_p0=random.random())for j in range(reps)]
+    return [pk.IV_Parameters(Q_pc=1,V_c=1,V_p=1,CL=1,q_c0=random.random(),q_p0=random.random())for j in range(reps)]
 
 def generateSubParams(reps: int):
-    return [pk.Subcut_Parameters(Q_pc=random.random(),V_c=random.random(),V_p=random.random(),CL=random.random(),k_a=random.random(),q_e0=random.random(),q_c0=random.random(),q_p0=random.random())for j in range(reps)]
+    return [pk.Subcut_Parameters(Q_pc=1,V_c=1,V_p=1,CL=1,k_a=1,q_e0=0,q_c0=0,q_p0=0)for j in range(reps)]
+
 
 
 def generateDoseFns(reps: int):
@@ -28,7 +29,7 @@ def generateDoseFns(reps: int):
 
 class ModelCompareTest(unittest.TestCase):
 
-    @parameterized.expand([(s1, s2, p, d, t, n) for s1 in ivSubjects for s2 in ivSubjects if s1!=s2 for p, d, t, n in zip(generateIVParams(reps), generateDoseFns(reps), [1 for j in range(reps)], [100 for j in range(reps)])])
+    @parameterized.expand([(s1, s2, p, d, t, n) for s1 in ivSubjects for s2 in ivSubjects if s1!=s2 for p, d, t, n in zip(generateIVParams(reps), generateDoseFns(reps), [random.random()/1000 for j in range(reps)], [random.randint(1000, 2000) for j in range(reps)])])
     def test_ivCompare(self, subject1: pk.AbstractModel, subject2: pk.AbstractModel, params: pk.AbstractParameters, doseFn: typing.Callable[[float], float], tSpan: float, numIters: int):
         sol1, sol2 = pk.NumpyDataCollector(), pk.NumpyDataCollector()
         testSubject1 = subject1(params, sol1, doseFn, tSpan, numIters)
@@ -37,7 +38,7 @@ class ModelCompareTest(unittest.TestCase):
         testSubject2.solve()
         self.helperCompare(sol1, sol2, numIters)
 
-    @parameterized.expand([(s1, s2, p, d, t, n) for s1 in subSubjects for s2 in subSubjects if s1!=s2 for p, d, t, n in zip(generateSubParams(reps), generateDoseFns(reps), [1 for j in range(reps)], [100 for j in range(reps)])])
+    @parameterized.expand([(s1, s2, p, d, t, n) for s1 in subSubjects for s2 in subSubjects if s1!=s2 for p, d, t, n in zip(generateSubParams(reps), generateDoseFns(reps), [random.random()/1000 for j in range(reps)], [random.randint(1000, 2000) for j in range(reps)])])
     def test_subCompare(self, subject1: pk.AbstractModel, subject2: pk.AbstractModel, params: pk.AbstractParameters, doseFn: typing.Callable[[float], float], tSpan: float, numIters: int):
         sol1, sol2 = pk.NumpyDataCollector(), pk.NumpyDataCollector()
         testSubject1 = subject1(params, sol1, doseFn, tSpan, numIters)
@@ -48,9 +49,8 @@ class ModelCompareTest(unittest.TestCase):
 
     def helperCompare(self, sol1, sol2, numIters):
         point1, point2 = sol1[numIters-1], sol2[numIters-1]
-        for i in range(1):#point1.shape[0]):
-            self.assertAlmostEqual(point1[i,0], point2[i,0], 0, "Models don't agree")
-
+        for i in range(point1.shape[0]):
+            self.assertAlmostEqual(point1[i,0], point2[i,0], 1, "Models don't agree")
 
 
     
