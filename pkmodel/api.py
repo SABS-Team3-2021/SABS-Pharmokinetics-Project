2702c3641f80cb22938668c4054d1cfb28ece9df
NicholasFan235 <30301848+NicholasFan235@users.noreply.github.com>
2021-10-21 16:30:33 +0100

    Added More Dosing Function Creaters to Api (#58)

--- pkmodel/api.py
+++ pkmodel/api.py
@@ -3,7 +3,7 @@ from .dataCollectors.dataCollector_numpy import NumpyDataCollector
 from .models.iv_model_scipy import IvModelScipy
 from .parameters.parameters_sub import Subcut_Parameters
 from .models.sub_model_scipy import SubModelScipy
-
+import numpy as np
 
 def solve_iv_toFile(outfilename,
         Q_pc=1, V_c=1, V_p=1, CL=1, q_c0=0, q_p0=0,
@@ -50,3 +50,33 @@ def solve_subcut_toFile(outfilename,
     model = SubModelScipy(params, soln, doseFn, tSpan, numIters)
     model.solve()
     soln.writeToFile(outfilename)
+
+
+def create_expDecay_dosing(A: float, k: float):
+    """ Create an Exponentially decaying dosing profile (A*exp(-k*t))
+    : param: A float: Dose at t=0
+    : param: k float: Dosing decay rate
+    : returns: Dosing Profile
+    : rtype: Callable[[float], float]
+    """
+    def inner(t: float) -> float:
+        return A * np.exp(-np.abs(k)*t)
+    return inner
+
+def create_periodic_dosing(timeHigh, timeLow, highVal, lowVal=0):
+    """ Create a Periodic dosing profile
+    # ------------------------------------------------------------
+    # Create a dosing profile which oscillates between high and low values.
+    # Remains high for timeHigh and low for lowHigh each period
+    # ------------------------------------------------------------
+    : param: timeHigh float: Time dosing remains at highVal
+    : param: timeLow float: Time dosing remains at lowVal
+    : param: highVal float: dosing ammount during high levels
+    : param: lowVal float: dosing level during low levels
+    : returns: Dosing Profile
+    : rtype: Callable[[float], float]
+    """
+    def inner(t: float) -> float:
+        phase = t%(timeHigh+timeLow)
+        return highVal if phase <= timeHigh else lowVal
+    return inner
