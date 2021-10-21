f06c67db87e9951c6a956a8ee0714286fcbdfa05
Patricia Lamirande <patricia.lamirande@dtc.ox.ac.uk>
2021-10-21 14:27:01 +0100

    Added documentation for Parameters classes

--- pkmodel/parameters/parameters_iv.py
+++ pkmodel/parameters/parameters_iv.py
@@ -1,30 +1,40 @@
+"""Module containing the pharmokinetics (PK) intravenous (IV) parameters class.
+
+It contains a class with a getParam and params_set methods.
+"""
+
 from ..abstractParameters import AbstractParameters
 
+
 class IV_Parameters(AbstractParameters):
-    '''
-    Class stores parameters specifically for the IV system
-    as a dictionary
-    '''
+    """Class in which the parameters for the PK IV model are stored as a
+    dictionary.
+
+    It inherits from AbstractParameters.
+    """
 
     def __init__(self, *args, **kwargs):
         expect_params = ['Q_pc', 'V_c', 'V_p', 'CL', 'q_c0', 'q_p0']
         for param in expect_params:
             assert param in kwargs, 'Missing parameter "{}"'.format(param)
-        self.params = {key : kwargs[key] for key in kwargs}
+        self.params = {key: kwargs[key] for key in kwargs}
         # assert len(self.params) == 4, 'Too many parameters for the IV model'
-            
+
     def getParam(self, key: str) -> float:
+        """Returns the parameter associated to a certain key in the dictionary.
+
+        :param key: name associated to the parameter
+        :return: parameter associated to the key
+        """
         assert key in self.params
         return self.params[key]
 
     def params_set(self) -> set:
-        '''
-        Returns a complete list of the the parameters 
-        '''
+        """Returns the complete list of parameters, in the form of a dictionary.
+        """
         return self.params.keys()
 
     def __str__(self) -> str:
-        '''
-        Return a string representation of the dictionary of paramters
-        '''
+        """Returns a string representation of the parameters' dictionary.
+        """
         return str(self.params)
--- pkmodel/parameters/parameters_sub.py
+++ pkmodel/parameters/parameters_sub.py
@@ -1,29 +1,41 @@
+"""Module containing the pharmokinetics (PK) subcutaneous (sub) parameters
+class.
+
+It contains a class with a getParam and params_set methods.
+"""
+
 from ..abstractParameters import AbstractParameters
 
+
 class Subcut_Parameters(AbstractParameters):
-    '''
-    Class stores parameters specifically for the IV system
-    as a dictionary
-    '''
+    """Class in which the parameters for the PK sub model are stored as a
+    dictionary.
+
+    It inherits from AbstractParameters.
+    """
 
     def __init__(self, *args, **kwargs):
-        expect_params = ['Q_pc', 'V_c', 'V_p', 'CL','k_a', 'q_c0', 'q_p0', 'q_e0']
+        expect_params = \
+            ['Q_pc', 'V_c', 'V_p', 'CL', 'k_a', 'q_c0', 'q_p0', 'q_e0']
         for param in expect_params:
             assert param in kwargs, 'Missing parameter "{}"'.format(param)
-        self.params = {key : kwargs[key] for key in kwargs}
-    
+        self.params = {key: kwargs[key] for key in kwargs}
+
     def getParam(self, key: str) -> float:
+        """Returns the parameter associated to a certain key in the dictionary.
+
+        :param key: name associated to the parameter
+        :return: parameter associated to the key
+        """
         assert key in self.params
         return self.params[key]
 
     def params_list(self) -> list:
-        '''
-        Returns a complete list of the the parameters 
-        '''
+        """Returns the complete list of parameters, in the form of a dictionary.
+        """
         return self.params.keys()
 
     def __str__(self) -> str:
-        '''
-        Return a string representation of the dictionary of paramters
-        '''
+        """Returns a string representation of the parameters' dictionary.
+        """
         return str(self.params)
