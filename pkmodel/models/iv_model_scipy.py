cb5fff0c3265f1db274da0b98b054749b825e0fb
Patricia Lamirande <patricia.lamirande@dtc.ox.ac.uk>
2021-10-21 10:55:06 +0100

    Added documentation in the models, and corrected a function name in sub model

--- pkmodel/models/iv_model_scipy.py
+++ pkmodel/models/iv_model_scipy.py
@@ -1,4 +1,9 @@
-# Model solving the intravenous bolus set of equations, using scipy.
+"""Module containing the pharmokinetics (PK) intravenous (IV) model class.
+
+It contains a class with a solve method, in which the equations
+of the model are defined and solved.
+"""
+
 import typing
 import numpy as np
 import scipy.integrate
@@ -9,7 +14,10 @@ from ..abstractDataCollector import AbstractDataCollector
 
 
 class IvModelScipy(AbstractModel):
-    """ A two compartment (intravenous) PK model
+    """Class in which the PK IV model is defined and solved.
+
+    It inherits from AbstractModel.
+    It contains the function solve and the subfunction pk_iv_model.
     """
 
     def __init__(self, parameters: AbstractParameters,
@@ -23,13 +31,16 @@ class IvModelScipy(AbstractModel):
         self.nsteps = nsteps
 
     def solve(self):
-        """ Solve IV problem and output to solution
+        """Solves the two compartments PK IV model and outputs to solution.
 
         It gets the parameters using the parameter class method.
+        The solver used is scipy.
 
-        It writes line by line the solution of the ODEs using
-        the solution class method. List format: [time, q_c, q_p].
+        It writes line by line the solution of the ODEs using the solution
+        class method.
+        Solution format: [time, q_c, q_p].
         """
+        # Definition of the parameters
         Q_pc = self.parameters.getParam("Q_pc")
         V_c = self.parameters.getParam("V_c")
         V_p = self.parameters.getParam("V_p")
@@ -38,18 +49,25 @@ class IvModelScipy(AbstractModel):
                               self.parameters.getParam("q_p0")]
         t_eval = np.linspace(0, self.timespan, self.nsteps)
 
+        # Definition of the model ODEs
         def pk_iv_model(t, y, Q_pc, V_c, V_p, CL):
-            """ Returns derivatives for PK IV Model
+            """Defines the differential equations for the PK IV model.
 
             Parameters:
-            Q_pc : transition rate between central and peripheral
-                   compartments (mL/h)
-            V_c : volume of central compartment (mL)
-            V_p : volume of peripheral compartment (mL)
-            CL : clearance/elimination rate from the central compartment (mL/h)
+            :param t: time (h)
+            :param y: list of the state variables of the ODEs system, in the
+                      form [q_c, q_p]
+            :param Q_pc: transition rate between central and peripheral
+                         compartments (mL/h)
+            :param V_c: volume of central compartment (mL)
+            :param V_p: volume of peripheral compartment (mL)
+            :param CL: clearance/elimination rate from the central
+                       compartment (mL/h)
 
-            Returns:
-            List containing differential equations, in the form:
+            The parameters (except for t and y) are extracted from the
+            Parameter class, using getParam method.
+
+            Returns list containing the differential equations, in the form:
             [dqc_dt, dqp_dt]
             """
             q_c, q_p = y
@@ -58,12 +76,14 @@ class IvModelScipy(AbstractModel):
             dqp_dt = transfer
             return [dqc_dt, dqp_dt]
 
+        # Solving the model
         sol = scipy.integrate.solve_ivp(
             fun=lambda t, y: pk_iv_model(t, y, Q_pc, V_c, V_p, CL),
             t_span=[t_eval[0], t_eval[-1]],
             y0=initial_conditions, t_eval=t_eval
         )
 
+        # Feeding the solution line by line to solution class
         t = sol.t
         y = sol.y
         N = t.shape[0]
