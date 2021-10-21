ed910d757e3a3b5e030441f7dbee0324981c9005
Luke Heirene <luke.heirene@dtc.ox.ac.uk>
2021-10-21 11:51:08 +0100

    Added pulse dose function

---
+++ pkmodel/dose_fns/dose_fns_blockpulse.py
@@ -0,0 +1,29 @@
+
+class blockpulse():
+
+    """ A block dosing function that can add multiple block pulses of a dose,
+        each dose pulse has a beginning time, end time and dose ammount
+    """
+    def __innit__(self):
+        self.pulses = {}
+   
+    def add_pulse(self, start_time, stop_time, dose) -> None:
+        """ Method to add individual pulses
+        """
+        self.pulses[start_time, stop_time] = dose
+        return
+
+    def __call__(self, x : float) -> float:
+        """ Calls dosing function to be used in solution of PK models
+        """
+        current = 0
+        for key in self.pulses:
+            pulseAmmount = self.pulses[key]
+            startTime = key[0]
+            stopTime=key[1]
+            if x >= startTime and x < stopTime:
+                current += pulseAmmount
+        return current
+
+
+   
